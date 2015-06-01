3c4cc5bcd9d6d8dfe9f38180da54241f1cad20ed
longvo <jackie@vietnamcubator.com>
2015-06-01 10:01:10 +0700

    migrate source from target web to hitsnl web

--- frontend/models/customer.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace frontend\models;
-
-use yii\db\ActiveRecord;
-
-class customer extends ActiveRecord{
-	
-}
-?>
--- frontend/views/site/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-\frontend\assets\LoginLayoutAsset::register($this);
-echo \backend\modules\loginwidget\widgets\LoginWidget::widget(['successUrl' => 'http://sdasds.com']);
-
